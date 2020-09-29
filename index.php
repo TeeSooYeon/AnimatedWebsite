e2a42fa33fad0939f98134f6315e854ca8fb3cea
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-09-29 10:56:20 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,196 @@
+<html>
+    <head>
+        <title>The Alps</title>
+        <link href="https://fonts.googleapis.com/css2?family=Carter+One&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Kufam:ital,wght@1,600&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
+        <link rel="icon" href="https://hgcamp.com/wp-content/uploads/2016/12/mountain-icon-512x512.png">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body id="head">
+        <ul>
+            <li><a class="button1" href="#head">Home</a></li>
+            <li><a class="button1" href="#gallery">Gallery</a></li>
+            <li style="float:right"><a class="button1" href="#footer">Contact Us</a></li>
+            <li style="float:right"><a class="button1" href="#about">More</a></li>
+            <div class="alps_logo">
+                <div class="snow">
+                    <div class="flake1"></div>
+                    <div class="flake2"></div>
+                    <div class="flake3"></div>
+                    <div class="flake4"></div>
+                    <div class="flake5"></div>
+                    <div class="flake6"></div>
+                    <div class="flake7"></div>
+                    <div class="flake8"></div>
+                </div>
+            <div class="mountain-1"></div><div class="mountain-2"></div>
+            <h1 class="header">ALPS</h1>
+            </div>
+        </ul>
+        <div class="wrapper">
+            <div class="snowflake">
+                <div>❅</div>
+                <div>❆</div>
+                <div>❅</div>
+                <div>❆</div>
+                <div>❅</div>
+                <div>❆</div>
+                <div>❅</div>
+                <div>❆</div>
+                <div>❅</div>
+                <div>❆</div>
+            </div>
+            <div class="info"></div>
+            <div class="piz-bernina">
+                <div class="mountain">
+                    <div class="mountain-top">
+                      <div class="mountain-cap-1"></div>
+                      <div class="mountain-cap-2"></div>
+                      <div class="mountain-cap-3"></div>
+                    </div>
+                </div>
+                  <div class="mountain-two">
+                    <div class="mountain-top">
+                      <div class="mountain-cap-1"></div>
+                      <div class="mountain-cap-2"></div>
+                      <div class="mountain-cap-3"></div>
+                    </div>
+                  </div>
+                   <div class="mountain-three">
+                    <div class="mountain-top">
+                      <div class="mountain-cap-1"></div>
+                      <div class="mountain-cap-2"></div>
+                      <div class="mountain-cap-3"></div>
+                    </div>
+                  </div>
+                  <div class="cloud"></div>
+                  <span class="m1">Piz Bernina <br/><br/>Piz Bernina is the highest mountain in the Eastern Alps, the highest point of the Bernina Range, and the highest peak in the Rhaetian Alps. It rises 4,048.6 m and is located south of Pontresina and near the major Alpine resort of St. Moritz, in the Engadin valley.</span>
+                </div>
+            <div class="weisshorn">
+                <div class="mountain2">
+                    <div class="mountain-top">
+                      <div class="mountain-cap-1"></div>
+                      <div class="mountain-cap-2"></div>
+                      <div class="mountain-cap-3"></div>
+                    </div>
+                </div>
+                <div class="land">
+                    <div class="tree">
+                        <div class="branch1"></div>
+                        <div class="branch2"></div>
+                        <div class="branch3"></div>
+                    </div>
+                    <div class="tree1">
+                        <div class="branch1"></div>
+                        <div class="branch2"></div>
+                        <div class="branch3"></div>
+                    </div>
+                    <div class="tree2">
+                        <div class="branch1"></div>
+                        <div class="branch2"></div>
+                        <div class="branch3"></div>
+                    </div>
+                    <div class="tree3">
+                        <div class="branch1"></div>
+                        <div class="branch2"></div>
+                        <div class="branch3"></div>
+                    </div>
+                </div>
+                <div class="cloud2"></div>
+                <span class="m2">Weisshorn <br/><br/>The Weisshorn is a major peak of Switzerland and the Alps, culminating at 4,506 metres above sea level. It is part of the Pennine Alps and is located between the valleys of Anniviers and Zermatt in the canton of Valais.</span>
+            </div>
+            <div class="matterhorn">
+                <div class="mountain3">
+                    <div class="mountain-top-2">
+                      <div class="mountain-cap-1-2"></div>
+                      <div class="mountain-cap-2-2"></div>
+                      <div class="mountain-cap-3-2"></div>
+                    </div>
+                </div>
+                <div class="mountain_small"></div>
+                <div class="mountain_small-2"></div>
+                <div class="mountain_small-3"></div>
+                <div class="mountain_small-4"></div>
+                <div class="cloud3"></div>
+                <span class="m3">Matterhorn<br/><br/>The Matterhorn, the main watershed and border between Switzerland and Italy. It is a large, near-symmetric pyramclassal peak in the extended Monte Rosa area of the Pennine Alps, whose summit is 4,478 metres high, making it one of the highest summits in the Alps and Europe.</span>
+            </div>
+        </div> 
+    </body>
+    <div id="footer" class="footer">
+        <div class="cont">
+            <div class='wave -one'></div>
+            <div class='wave -two'></div>
+            <div class='wave -three'></div>
+        </div>
+        <div class="cont1">
+            <div class='wave -one'></div>
+            <div class='wave -two'></div>
+            <div class='wave -three'></div>
+        </div>
+        <div class="cont2">
+            <div class='wave -one'></div>
+            <div class='wave -two'></div>
+            <div class='wave -three'></div>
+        </div>
+        <div class="cont3">
+            <div class='wave -one'></div>
+            <div class='wave -two'></div>
+            <div class='wave -three'></div>
+        </div>
+        <div class="cont4">
+            <div class='wave -one'></div>
+            <div class='wave -two'></div>
+            <div class='wave -three'></div>
+        </div>
+        <div class="cont5">
+            <div class='wave -one'></div>
+            <div class='wave -two'></div>
+            <div class='wave -three'></div>
+        </div>
+        <div class="cont6">
+            <div class='wave -one'></div>
+            <div class='wave -two'></div>
+            <div class='wave -three'></div>
+        </div>
+        <div class="facebook_cont">
+            <div class="facebook-icon"></div>
+        </div>
+        <div class="insta">
+            <div class="container">
+                <div class="shape"></div>
+            </div>
+        </div>
+        <div class="youtube">
+            <div class = "logo">
+                <div class = "red">
+                    <div class = "play"></div>
+                </div>
+            </div>
+        </div>
+        <div class="twt">
+            <div class="logo-container">
+                <div class="sar"></div>
+                <div class="gardan"></div>
+                <div class="par1"></div>
+                <div class="par1-overlay"></div>
+                <div class="par2"></div>
+                <div class="par2-overlay"></div>
+                <div class="par3"></div>
+                <div class="par3-overlay"></div>
+                <div class="par4"></div>
+                <div class="par4-overlay"></div>
+                <div class="nok-top"></div>
+                <div class="nok-top-overlay"></div>
+                <div class="nok-bottom"></div>
+                <div class="nok-bottom-overlay"></div>
+            </div>
+        </div>
+        <span class="footer_txt">The Alps by Hans Toquero</span>
+        <span class="footer_txt_2">Copyright © 2020.All rights Reserved</span>
+        <span class="footer_txt_3">Follow Us:</span>
+
+        <span class="footer_txt_4"><a href="https://github.com/TeeSooYeon/AnimatedWebsite_theAlps" target="_blank"><i style="font-size: 24px;" class="fa fa-github"></i></a></span>
+    </div>
+</html>
